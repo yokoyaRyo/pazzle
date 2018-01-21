ffa7967f4cf1f483a8217a583a765a40228c353e
涼 <@zero>
2018-01-21 19:19:29 +0900

    修正、bootstrap導入

--- WebContent/public/main.js
+++ WebContent/public/main.js
@@ -4,38 +4,100 @@ function StartGame() {
 	var thisElement = event.target;
 	var line = thisElement.getAttribute('data-line');
 	var column = thisElement.getAttribute('data-column');
-
 	var body = document.body;
-	var menuElement = document.getElementById('menu');
+
 	menuElement.setAttribute('style', 'display: none');
 
 	CreateField(line, column, body);
-
 	SetBlock('gameTable', line, column, 2);
-
 	SetBlock('gameTable', line, column, 2);
-
+	SetTableColor(line, column);
+	CalcRank()
 }
+
+function SetTableColor(side, vertical) {
+	for (let i = 0; i < vertical; i++) {
+		for (let j = 0; j < side; j++) {
+			var tdTag = document.getElementById('gameTable' + 'Side' + (i + 1) + 'gameTable' + 'Vertical' + (j + 1));
+			console.log(tdTag);
+			SetTdColor(tdTag);
+			if('block' + tdTag){
+				tdTag.setAttribute('style', 'font-size: 26pt;');
+			}
+		}
+	}
+}
+
+function SetTdColor(td) {
+	var color;
+	console.log(td.getAttribute('data-block'));
+	if (td.getAttribute('data-block') == 2) {
+		console.log('2');
+		color = 'yellow';
+	}
+	if (td.getAttribute('data-block') == 4) {
+		color = 'gold';
+	}
+	if (td.getAttribute('data-block') == 8) {
+		color = 'orange';
+	}
+	if (td.getAttribute('data-block') == 16) {
+		color = 'darkorange';
+	}
+	if (td.getAttribute('data-block') == 32) {
+		color = 'chocolate';
+	}
+	if (td.getAttribute('data-block') == 64) {
+		color = 'red';
+	}
+	if (td.getAttribute('data-block') == 128) {
+		color = 'violet';
+	}
+	if (td.getAttribute('data-block') == 256) {
+		color = 'darkorchid';
+	}
+	if (td.getAttribute('data-block') == 512) {
+		color = 'purple';
+	}
+	if (td.getAttribute('data-block') == 1024) {
+		color = 'darkslateblue';
+	}
+	if (td.getAttribute('data-block') == 2048) {
+		color = 'blue';
+	}
+	if (td.getAttribute('data-block') == 4096) {
+		color = 'springgreen';
+	}
+	if (td.getAttribute('data-block') >= 8192) {
+		color = 'lime';
+	}
+
+	td.setAttribute('bgcolor', color);
+}
+
 
 function ContinuGame() {
 	StartGame();
 }
 
 function CreateField(line, column, body) {
-	var gameElement = CreateElement('div', 'gameElement');
-	AddElement(body, gameElement);
+//	var gameElement = CreateElement('div', 'gameElement');
+//	AddElement(body, gameElement);
 
+	var gameElement = document.getElementById('gameElement');
+	gameElement.removeAttribute('style');
+
+	var table = document.getElementById('table');
 	var game = CreateElement('table', 'gameTable');
 	game.setAttribute('border', '1');
-	game.setAttribute('width', '30%');
-	game.setAttribute('height', '30%');
+	game.setAttribute('width', '400px');
+	game.setAttribute('height', '400px');
 	game.setAttribute('style', 'margin: 0 auto;');
 	game.setAttribute('data-line', line);
 	game.setAttribute('data-column', column);
-	AddElement(gameElement, game);
+	AddElement(table, game);
 	CreateGameTable(game, game.getAttribute('id'), line, column);
 	TableSize('gameTable', column, line);
-
 
 	var operation = CreateElement('table', 'operationTable');
 	operation.setAttribute('border', '1');
@@ -44,33 +106,24 @@ function CreateField(line, column, body) {
 	operation.setAttribute('style', 'margin: 0 auto;');
 	AddElement(gameElement, operation);
 	CreateGameTable(operation, operation.getAttribute('id'), 3, 3);
-	operation.setAttribute('style', 'display: none');
 
+	operation.setAttribute('style', 'display: none');
 	CreateOperationButton(operation.getAttribute('id'), 1, 2, 'Move()', '↑', line, column, 'up');
 	CreateOperationButton(operation.getAttribute('id'), 2, 1, 'Move()', '←', line, column, 'left');
 	CreateOperationButton(operation.getAttribute('id'), 2, 3, 'Move()', '→', line, column, 'right');
 	CreateOperationButton(operation.getAttribute('id'), 3, 2, 'Move()', '↓', line, column, 'under');
 
-	var rankNumber = CreateElement('div', 'rankNumber');
+	var ten = document.getElementById('ten');
 	var number = CreateTextNode(4);
-	rankNumber.setAttribute('data-num', 4);
-	AddElement(rankNumber, number);
-	AddElement(gameElement, rankNumber);
+	ten.setAttribute('data-num', 4);
+	AddElement(ten, number);
 
-	var saveButton = CreateButton('div', 'save', 'メニューに戻る', gameElement);
-	saveButton.setAttribute('onclick', 'ReturnMenu()');
 }
 
 function Rank() {
-//	document.getElementById('save').setAttribute('data-menu', menuElement);
-
 	var menuElement = document.getElementById('menu');
 	menuElement.setAttribute('style', 'display: none');
-
-	var rankPage = CreateElement('div', 'rankPage');
-	AddElement(document.body, rankPage);
-	var saveButton = CreateButton('div', 'return', 'メニューに戻る', rankPage);
-	saveButton.setAttribute('onclick', 'ReturnMenu2()');
+	document.getElementById('rankPage').removeAttribute('style');
 
 	$.ajax({
 	    url: 'http://localhost:8080/Pazzle/PazzleServlet',
@@ -79,8 +132,7 @@ function Rank() {
 	    success: function (data) {
 	    	console.log("読み込み成功");
 
-
-	    	var tableTag = CreateElement('table', 'rankTable');
+	    	var tableTag = document.getElementById('rankTable');
 	    	var count = data.length;
 	    	for(let i = 0; i < count; i++) {
 
@@ -103,9 +155,6 @@ function Rank() {
     			playerTag.textContent = record.player;
 
 	    	}
-
-	    	AddElement(document.getElementById('rankPage'), tableTag);
-
 
 	    },
 	    error: function (data) {
@@ -216,46 +265,46 @@ function CreateBlock(id, number) {
 function SetColor(block, number) {
 	var color;
 	if (number == 2) {
-		color = 'yellow';
-	}
-	if (number == 4) {
-		color = 'gold';
-	}
-	if (number == 8) {
-		color = 'orange';
-	}
-	if (number == 16) {
-		color = 'darkorange';
-	}
-	if (number == 32) {
 		color = 'chocolate';
 	}
-	if (number == 64) {
+	if (number == 4) {
 		color = 'red';
 	}
-	if (number == 128) {
+	if (number == 8) {
 		color = 'violet';
 	}
-	if (number == 256) {
+	if (number == 16) {
 		color = 'darkorchid';
 	}
-	if (number == 512) {
+	if (number == 32) {
 		color = 'purple';
 	}
-	if (number == 1024) {
+	if (number == 64) {
 		color = 'darkslateblue';
 	}
-	if (number == 2048) {
+	if (number == 128) {
 		color = 'blue';
 	}
-	if (number == 4096) {
+	if (number == 256) {
 		color = 'springgreen';
 	}
-	if (number >= 8192) {
+	if (number == 512) {
 		color = 'lime';
 	}
+	if (number == 1024) {
+		color = 'yellow';
+	}
+	if (number == 2048) {
+		color = 'gold';
+	}
+	if (number == 4096) {
+		color = 'orange';
+	}
+	if (number >= 8192) {
+		color = 'darkorange';
+	}
 
-//	block.setAttribute('style', 'font-weight: 700');
+
 	block.setAttribute('style', 'color: ' + color);
 	block.setAttribute('align', 'center');
 	block.setAttribute('data-num', number);
@@ -268,21 +317,27 @@ function SetBlock(id, line, column, number) {
 }
 
 function ReturnMenu() {
-	DeleteElement(document.body, document.getElementById('gameElement'));
+	document.getElementById('gameElement').setAttribute('style', 'display: none');
+	DeleteElement(document.getElementById('table'), document.getElementById('gameTable'));
+	DeleteElement(document.getElementById('gameElement'), document.getElementById('operationTable'));
+	if(document.getElementById('gameOver')) {
+		DeleteElement(document.getElementById('gameover'), document.getElementById('gameOver'));
+		DeleteElement(document.getElementById('gameover'), document.getElementById('inputPlayer'));
+		DeleteElement(document.getElementById('gameover'), document.getElementById('form'));
+	}
 	document.getElementById('menu').setAttribute('style', '');
 }
 
 function ReturnMenu2() {
-	DeleteElement(document.body, document.getElementById('rankPage'));
+	document.getElementById('rankPage').setAttribute('style', 'display: none');
+	//DeleteElement(document.body, document.getElementById('rankPage'));
 	document.getElementById('menu').setAttribute('style', '');
 }
 
 function InsertRank() {
 
-	var num = document.getElementById('rankNumber').getAttribute('data-num');
+	var num = document.getElementById('ten').getAttribute('data-num');
 	var player = document.forms.form.textBox.value;
-
-	console.log(num + player);
 
 	$.ajax({
 	type: 'POST',
@@ -360,6 +415,7 @@ function LeftMotion(line, column) {
 	}
 	TableSize('gameTable', column, line);
 	if (count == line) {
+		SetTableColor(line, column);
 		return;
 	}
 	game = document.getElementById('gameTable');
@@ -368,6 +424,7 @@ function LeftMotion(line, column) {
 	CalcRank();
 
 	var flag = MoveFlag(line, column);
+	SetTableColor(line, column);
 	GameOver(flag, line, column);
 }
 
@@ -408,6 +465,7 @@ function RightMotion(line, column) {
 	}
 	TableSize('gameTable', column, line);
 	if (count == line) {
+		SetTableColor(line, column);
 		return;
 	}
 	game = document.getElementById('gameTable');
@@ -416,8 +474,8 @@ function RightMotion(line, column) {
 	CalcRank();
 
 	var flag = MoveFlag(line, column);
+	SetTableColor(line, column);
 	GameOver(flag, line, column);
-
 }
 
 function UpMotion(line, column) {
@@ -460,6 +518,7 @@ function UpMotion(line, column) {
 	}
 	TableSize('gameTable', column, line);
 	if (count == column) {
+		SetTableColor(line, column);
 		return;
 	}
 	game = document.getElementById('gameTable');
@@ -468,6 +527,7 @@ function UpMotion(line, column) {
 	CalcRank();
 
 	var flag = MoveFlag(line, column);
+	SetTableColor(line, column);
 	GameOver(flag, line, column);
 }
 
@@ -478,7 +538,6 @@ function UnderMotion(line, column) {
 	var stockArray = [];
 
 	for (let countColumn = 1; countColumn <= column; countColumn++) {
-
 		blockArray = CreateBlockArrayUnderMotion(countColumn, line);
 		stockArray = blockArray;
 		blockArray = stockArray.concat();
@@ -511,6 +570,7 @@ function UnderMotion(line, column) {
 	}
 	TableSize('gameTable', column, line);
 	if (count == column) {
+		SetTableColor(line, column);
 		return;
 	}
 	game = document.getElementById('gameTable');
@@ -519,25 +579,24 @@ function UnderMotion(line, column) {
 	CalcRank();
 
 	var flag = MoveFlag(line, column);
+	SetTableColor(line, column);
 	GameOver(flag, line, column);
 }
 
 function CalcRank() {
-	var num = parseInt(document.getElementById('rankNumber').getAttribute('data-num'));
-	DeleteElement(document.getElementById('gameElement'), document.getElementById('rankNumber'));
-	DeleteElement(document.getElementById('gameElement'), document.getElementById('save'));
+	var rankNumber = document.getElementById('rankNumber');
+	var ten = document.getElementById('ten');
 
+	var num = parseInt(ten.getAttribute('data-num'));
+	console.log(rankNumber.getAttribute('data-num'));
 
-	var rankNumber = CreateElement('div', 'rankNumber');
-	var number = CreateTextNode(num + 2);
-	rankNumber.setAttribute('data-num', num + 2);
-	AddElement(rankNumber, number);
-	AddElement(gameElement, rankNumber);
+	DeleteElement(rankNumber, ten);
+	ten = CreateElement('div', 'ten');
+	AddElement(rankNumber, ten);
+	ten.setAttribute('data-num', num + 2);
 
-	var saveButton = CreateButton('div', 'save', 'メニューに戻る', gameElement);
-	saveButton.setAttribute('onclick', 'ReturnMenu()');
-
-
+	var node = CreateTextNode(num + 2);
+	AddElement(ten, node);
 }
 
 
@@ -545,31 +604,29 @@ function GameOver(flag, line, column) {
 	if(flag == (line*column)) {
 		console.log('gameover');
 
-		DeleteElement(document.getElementById('gameElement'), document.getElementById('save'));
+//		DeleteElement(document.getElementById('gameElement'), document.getElementById('save'));
 
-		var div = CreateElement('div', 'div');
-		AddElement(document.getElementById('gameElement'), div);
+//		var div = CreateElement('div', 'div');
+//		AddElement(document.getElementById('gameElement'), div);
 
 		var gameOver = CreateElement('div', 'gameOver');
 		var text = CreateTextNode('GAMEOVER');
 		AddElement(gameOver, text);
-		AddElement(div, gameOver);
+		AddElement(document.getElementById('gameover'), gameOver);
 
 		var popup = CreateElement('div', 'inputPlayer');
 		text = CreateTextNode('名前を入力してください');
 		AddElement(popup, text);
-		AddElement(div, popup);
-
-
+		AddElement(gameover, popup);
 
 		var form = CreateElement('form', 'form');
 		var textBox = CreateElement('input', 'textBox');
 		textBox.setAttribute('type', 'text');
-		AddElement(div, form);
+		AddElement(gameover, form);
 		AddElement(form, textBox);
 
-		var saveButton = CreateButton('div', 'save', 'メニューに戻る', gameElement);
-		saveButton.setAttribute('onclick', 'InsertRank()');
+//		var saveButton = CreateButton('div', 'save', 'メニューに戻る', gameElement);
+//		saveButton.setAttribute('onclick', 'InsertRank()');
 
 	}
 }
@@ -597,29 +654,21 @@ function MoveFlag(line, column) {
 	var flag = true;
 
 	for (countLine = 1; countLine <= line; countLine +=1) {
-
 		for (countColumn = 1; countColumn <= line; countColumn +=1) {
-
 			id = "gameTableSide" + countLine + "gameTableVertical" + countColumn;
-
 			if(document.getElementById(id).getAttribute('data-block') == null) {
 				flag = false;
 				break;
 			}
-
 		}
 		if(flag == false) {
 			break;
 		}
-
 	}
-
 
 	if(flag == true) {
 		for (countLine = 1; countLine <= line; countLine +=1) {
-
 			for (countColumn = 1; countColumn <= line; countColumn +=1) {
-
 				if(document.getElementById("gameTableSide" + countLine + "gameTableVertical" + countColumn).getAttribute('data-block') !== null) {
 					console.log(countLine);
 					console.log(countColumn);
@@ -628,7 +677,6 @@ function MoveFlag(line, column) {
 					underId = "blockgameTableSide" + (countLine+1) + "gameTableVertical" + countColumn;
 					leftId = "blockgameTableSide" + countLine + "gameTableVertical" + (countColumn-1);
 					rightId = "blockgameTableSide" + countLine + "gameTableVertical" + (countColumn+1);
-
 
 					if((countLine == 1) && (countColumn == 1)){
 						idNum = document.getElementById(id).getAttribute('data-num');
@@ -675,14 +723,12 @@ function MoveFlag(line, column) {
 						leftIdNum = document.getElementById(leftId).getAttribute('data-num');
 
 					} else {
-							idNum = document.getElementById(id).getAttribute('data-num');
-							upIdNum = document.getElementById(upId).getAttribute('data-num');
-							underIdNum = document.getElementById(underId).getAttribute('data-num');
-							leftIdNum = document.getElementById(leftId).getAttribute('data-num');
-							rightIdNum = document.getElementById(rightId).getAttribute('data-num');
-
+						idNum = document.getElementById(id).getAttribute('data-num');
+						upIdNum = document.getElementById(upId).getAttribute('data-num');
+						underIdNum = document.getElementById(underId).getAttribute('data-num');
+						leftIdNum = document.getElementById(leftId).getAttribute('data-num');
+						rightIdNum = document.getElementById(rightId).getAttribute('data-num');
 					}
-
 				}
 				console.log(idNum);
 				console.log(upIdNum);
@@ -698,15 +744,12 @@ function MoveFlag(line, column) {
 				underIdNum = 0;
 				leftIdNum = 0;
 				rightIdNum = 0;
-
 			}
-
 		}
 	}
 	console.log(count);
 
 	return count;
-
 }
 
 function CreateBlockArrayLeftMotion(countLine, column) {
